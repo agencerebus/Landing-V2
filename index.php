381b620c9820753ad79b3dff05307ace5dd883f2
unknown <richard.rabillon@live.fr>
2015-07-07 18:08:05 +0200

    Premier commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+	<head>
+        <meta charset="utf-8">
+        <title>Ariew</title>
+        <meta name="description" content="Le masque ARIEW permet d’enrichir l’expérience des utilisateurs sans dénaturer leur environnement.">
+        <meta name="author" content="Agence Rébus">
+        <meta name="viewport" content="initial-scale=1">
+        <link href="css/normalize.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <!-- Library jquery -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/ajax.js"></script>
+        <!-- FAVICON -->
+        <link rel="icon" type="image/x-icon" href="img/common/favicon.ico">
+        <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+
+    <body> 
+
+
+        
+
+
+
+    </body>
+
+    <footer>
+        <p>© Rébus | Contributor's Mazwai/Videezy.com</p>
+    </footer>
+
+</html>
